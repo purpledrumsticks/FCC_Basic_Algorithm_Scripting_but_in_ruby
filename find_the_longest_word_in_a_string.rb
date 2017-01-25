0c82d93a34fd6e734d2dce391e174f5dc05eeb91
Corey Slaven <coreyslaven@aol.com>
2017-01-25 10:44:25 -0500

    Found the longest word in a string! and then made an edit :)

--- find_the_longest_word_in_a_string.rb
+++ find_the_longest_word_in_a_string.rb
@@ -1,6 +1,3 @@
 def find_longest_word (str)
-  the_word = str.split(' ').max_by { |word| word.length }
-  the_word.length
+  str.split(' ').max_by { |word| word.length }.length
 end
-
-find_longest_word("What if we try a super-long word such as otorhinolaryngology")
